940babea71f97615eacfb253a42ecd01a620e8d4
ruchi06 <ruchi06@gmail.com>
2022-12-03 21:48:36 +0530

    Using floats find Max

--- MaximumNumberUsingGenerics/MaximumNumberUsingGenerics/MaximumNumber.cs
+++ MaximumNumberUsingGenerics/MaximumNumberUsingGenerics/MaximumNumber.cs
@@ -32,5 +32,28 @@ namespace MaximumNumberUsingGenerics
             }
             return Number3;
         }
+
+        internal static float MaximumfloatIntegerNumber(float Number1, float Number2, float Number3)
+        {
+            if (Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) > 0 ||
+               Number1.CompareTo(Number2) >= 0 && Number1.CompareTo(Number3) > 0 ||
+               Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) >= 0)
+            {
+                return Number1;
+            }
+            if (Number2.CompareTo(Number1) > 0 && Number2.CompareTo(Number3) > 0 ||
+                Number2.CompareTo(Number1) >= 0 && Number2.CompareTo(Number3) > 0 ||
+                Number2.CompareTo(Number1) > 0 && Number2.CompareTo(Number3) >= 0)
+            {
+                return Number2;
+            }
+            if (Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) > 0 ||
+                Number3.CompareTo(Number1) >= 0 && Number3.CompareTo(Number2) > 0 ||
+                Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) >= 0)
+            {
+                return Number3;
+            }
+            return Number1;
+        }
     }
 }
--- MaximumNumberUsingGenerics/MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/MaximumNumberUsingGenerics/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumIntegerNumber(10, 20, 30));
+            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumfloatIntegerNumber(20.10f, 10.50f, 30.68f));
         }
     }
 
