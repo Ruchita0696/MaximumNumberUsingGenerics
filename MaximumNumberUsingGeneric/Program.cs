083525e2edf2a6847a72548b90713facbbe9f7b6
ruchi06 <ruchi06@gmail.com>
2023-01-16 18:01:22 +0530

    Max float Number at Position 2

--- MaximumNumberUsingGeneric/Program.cs
+++ MaximumNumberUsingGeneric/Program.cs
@@ -6,7 +6,7 @@ namespace MaximumNumberUsingGeneric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumfloategerNumber(30.68f,10.50f,20.10f));
+            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumfloategerNumber(10.50f, 30.68f, 20.10f));
         }
 
     }
