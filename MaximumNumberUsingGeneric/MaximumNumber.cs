4def470336f6b8ef01c89823de74bc93a12cf615
ruchi06 <ruchi06@gmail.com>
2023-01-16 19:36:59 +0530

    Using more then three Parameters

--- MaximumNumberUsingGeneric/MaximumNumber.cs
+++ MaximumNumberUsingGeneric/MaximumNumber.cs
@@ -6,8 +6,53 @@ using System.Threading.Tasks;
 
 namespace MaximumNumberUsingGeneric
 {
-    public class MaximumNumber
+    public class MaximumNumber<M> where M : IComparable
     {
+        public M value1, value2, value3, value4;
+        public MaximumNumber(M value1, M value2, M value3, M value4)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+            this.value4 = value4;
+        }
+        public static M MaxNumber<M>(M value1, M value2, M value3, M value4) where M : IComparable
+        {
+            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 && value1.CompareTo(value4) > 0 ||
+                value1.CompareTo(value2) >= 0 && value1.CompareTo(value3) > 0 && value1.CompareTo(value4) > 0 ||
+                value1.CompareTo(value2) > 0 && value1.CompareTo(value3) >= 0 && value1.CompareTo(value4) > 0 ||
+                value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 && value1.CompareTo(value4) >= 0)
+            {
+                return value1;
+            }
+            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0 && value2.CompareTo(value4) > 0 ||
+                           value2.CompareTo(value1) >= 0 && value2.CompareTo(value3) > 0 && value2.CompareTo(value4) > 0 ||
+                           value2.CompareTo(value1) > 0 && value2.CompareTo(value3) >= 0 && value2.CompareTo(value4) > 0 ||
+                           value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0 && value2.CompareTo(value4) >= 0)
+            {
+                return value2;
+            }
+            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0 && value3.CompareTo(value4) > 0 ||
+                value3.CompareTo(value1) >= 0 && value3.CompareTo(value2) > 0 && value3.CompareTo(value4) > 0 ||
+                value3.CompareTo(value1) > 0 && value3.CompareTo(value2) >= 0 && value3.CompareTo(value4) > 0 ||
+                value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0 && value3.CompareTo(value4) >= 0)
+            {
+                return value3;
+            }
+            if (value4.CompareTo(value1) > 0 && value4.CompareTo(value2) > 0 && value4.CompareTo(value3) > 0 ||
+                value4.CompareTo(value1) >= 0 && value4.CompareTo(value2) > 0 && value4.CompareTo(value3) > 0 ||
+                value4.CompareTo(value1) > 0 && value4.CompareTo(value2) >= 0 && value4.CompareTo(value3) > 0 ||
+                value4.CompareTo(value1) > 0 && value4.CompareTo(value2) > 0 && value4.CompareTo(value3) >= 0)
+            {
+                return value4;
+            }
+            return default;
+        }
+        public M MaxMethod()
+        {
+            M Max = MaximumNumber<M>.MaxNumber(this.value1, this.value2, this.value3, this.value4);
+            return Max;
+        }
         public static int MaximumIntegerNumber(int Number1, int Number2, int Number3)
         {
             if (Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) > 0 ||
@@ -23,12 +68,12 @@ namespace MaximumNumberUsingGeneric
                 return Number2;
             }
             if (Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) > 0 ||
-                Number3.CompareTo(Number1) >= 0 && Number3.CompareTo(Number2) > 0 ||
-                Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) >= 0)
+               Number3.CompareTo(Number1) >= 0 && Number3.CompareTo(Number2) > 0 ||
+               Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) >= 0)
             {
                 return Number3;
             }
-            return Number3;
+            return Number1;
         }
         public static float MaximumfloatNumber(float Number1, float Number2, float Number3)
         {
@@ -74,6 +119,5 @@ namespace MaximumNumberUsingGeneric
             }
             return String1;
         }
-
     }
 }
--- MaximumNumberUsingGeneric/Program.cs
+++ MaximumNumberUsingGeneric/Program.cs
@@ -1,15 +1,18 @@
-﻿using static MaximumNumberUsingGeneric.MaximumNumber;
+﻿using System;
 
 namespace MaximumNumberUsingGeneric
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumIntegerNumber(20, 10, 30));
-            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumfloatNumber(20.10f, 10.50f, 30.68f));
-            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumstringNumber("Apple", "Peach", "Banana"));
-        }
 
+            MaximumNumber<int> maximumInt = new MaximumNumber<int>(30, 20, 10, 40);
+            Console.WriteLine("Maximum Number is : " + maximumInt.MaxMethod());
+            MaximumNumber<float> maximumFloat = new MaximumNumber<float>(20.10f, 40.95f, 10.50f, 30.68f);
+            Console.WriteLine("Maximum Number is : " + maximumFloat.MaxMethod());
+            MaximumNumber<string> maximumString = new MaximumNumber<string>("20", "30", "40", "10");
+            Console.WriteLine("Maximum Number is : " + maximumString.MaxMethod());
+        }
     }
 }
