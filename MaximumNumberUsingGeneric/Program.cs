4b6b54c460119b02aa665fca286ceb8e44b9f3e9
ruchi06 <ruchi06@gmail.com>
2023-01-16 20:52:47 +0530

    Refactor 1

---
+++ MaximumNumberUsingGeneric/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MaximumNumberUsingGeneric
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumNumber maximum = new MaximumNumber();
+
+            Console.WriteLine("Maximum Interger Number is : " + maximum.MaxNumber<int>(30, 20, 10));
+            Console.WriteLine("Maximum Float Number is : " + maximum.MaxNumber<float>(30, 20, 10));
+            Console.WriteLine("Maximum String Number is : " + maximum.MaxNumber<string>("44", "30", "10"));
+        }
+    }
+}
