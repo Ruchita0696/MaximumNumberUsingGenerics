d3c5f467076eda7e18d11784e0abf7ab54bebd22
ruchi06 <ruchi06@gmail.com>
2023-01-16 17:39:10 +0530

    Using Integer

--- MaximumNumberUsingGeneric/Program.cs
+++ MaximumNumberUsingGeneric/Program.cs
@@ -6,12 +6,7 @@ namespace MaximumNumberUsingGeneric
     {
         static void Main(string[] args)
         {
-            MaximumNumber<int> maximumInt = new MaximumNumber<int>(30, 20, 10);
-            Console.WriteLine("Maximum Number is : " + maximumInt.MaxMethod());
-            MaximumNumber<float> maximumFloat = new MaximumNumber<float>(10, 20, 30);
-            Console.WriteLine("Maximum Number is : " + maximumFloat.MaxMethod());
-            MaximumNumber<string> maximumString = new MaximumNumber<string>("20", "30", "10");
-            Console.WriteLine("Maximum Number is : " + maximumString.MaxMethod());
+            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumIntegerNumber(30, 20, 10));
         }
 
     }
