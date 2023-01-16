81e83964d1b07742fb0858dc3cc15ad35e35de76
ruchi06 <ruchi06@gmail.com>
2023-01-16 12:36:30 +0530

    Refector 1

---
+++ MaximumNumberUsingGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MaximumNumberUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
