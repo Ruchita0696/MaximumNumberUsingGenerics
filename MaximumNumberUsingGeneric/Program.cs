3de80dfae9d921e708d4acd624d3ef394baf9d1c
ruchi06 <ruchi06@gmail.com>
2023-01-16 17:49:45 +0530

    Max Number at Position 3

--- MaximumNumberUsingGeneric/Program.cs
+++ MaximumNumberUsingGeneric/Program.cs
@@ -6,7 +6,7 @@ namespace MaximumNumberUsingGeneric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumIntegerNumber(20, 30, 10));
+            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumIntegerNumber(20, 10, 30));
         }
 
     }
