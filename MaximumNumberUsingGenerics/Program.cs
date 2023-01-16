14d781f57489dc1d999eb8b40fc73f6ca478c550
ruchi06 <ruchi06@gmail.com>
2023-01-16 12:43:42 +0530

    Refector 1

--- MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MaximumNumber maximum = new MaximumNumber();
+
+            Console.WriteLine("Maximum Interger Number is : " + MaximumNumber.MaxNumber(30, 20, 10));
+            Console.WriteLine("Maximum Float Number is : " + MaximumNumber.MaxNumber<float>(30, 20, 10));
+            Console.WriteLine("Maximum String Number is : " + MaximumNumber.MaxNumber("44", "30", "10"));
         }
     }
+    
 }
